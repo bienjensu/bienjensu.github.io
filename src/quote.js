61d6815b87e675b7aa21cf50c96f5b7ab50756ff
Elizabeth <edw@sdf.org>
2025-06-24 13:47:20 +0100

    Adjust marquee

--- src/quote.js
+++ src/quote.js
@@ -14,35 +14,35 @@ var song = Array(
     `great thoughts spring from the heart<br>
 - Luc de Clapiers
 `,
-    `we must welcome big concepts<br>
+    `we must welcome big concepts
 - Grace Murray Hopper`,
-    `every rupture begins through a rupture with oneself<br>
+    `every rupture begins through a rupture with oneself
 - Alain Badiou`,
-    `there is physical reality and there is the miracle of explanation<br>
+    `there is physical reality and there is the miracle of explanation
 - Albert Lautman`,
-    `one more effort if you wish to become republicans<br>
+    `one more effort if you wish to become republicans
 - Donatien Alphonse François`,
-    `you don't want to know<br>
+    `you don't want to know
 - Richard Nixon`,
-    `you may think you know everything. don't<br>
+    `you may think you know everything. don't
 - Jacques Lacan`,
-    `that faith be analyzable does not necessarily imply a method for getting by without it<br>
+    `that faith be analyzable does not necessarily imply a method for getting by without it
 - Julia Kristeva`,
-    `i am against any reconciliation with Israel<br>
+    `i am against any reconciliation with Israel
 - Sayyed Hassan Nasrallah`,
-    `i want a poet to break out of his or her poetic identity<br>
+    `i want a poet to break out of his or her poetic identity
 - Jeremy Halvard Prynne`,
-    `the sea rises insensibly in silence<br>
+    `the sea rises insensibly in silence
 - Alexandre Grothendieck`,
-    `i am disappearing<br>
+    `i am disappearing
 - Jacques Lacan`,
-    `against the disease of writing one must take special precautions<br>
+    `against the disease of writing one must take special precautions
 - Peter Abelard`,
-    `what is fixed is dead, what is not fixed is nothing<br>
+    `what is fixed is dead, what is not fixed is nothing
 - Paul Valéry`,
-    `what appears as the positive is essentially the negative<br>
+    `what appears as the positive is essentially the negative
 - Theodore Adorno`
 )
 
 var randomSong = song[Math.floor(Math.random() * song.length)];
-document.getElementsByClassName('quoteblock')[0].innerHTML = randomSong;
+document.getElementsByClassName('quoteblock')[0].textContent = randomSong;
